39959d86eabbc78bc308afaffae9cbe4fe7af811
kiprudroid <kiprutoshadrack06@gmail.com>
2024-04-07 10:17:45 +0300

    Fixed Drawing

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ let curr;
 var box1 = document.getElementById("box1");
 var check = document.getElementById("check");
 var game = document.getElementById("game");
-var all = document.querySelectorAll("button");
-let hasNull;
+var all = document.querySelectorAll(".box");
+let isDraw;
+
 
 
 
 //box.addEventListener("click",choose,false)
-var arr = []
+var arr = [1,1,1,1,1,1,1,1,1]
         
 
 var arr1 = [0,1,2,
@@ -16,32 +17,56 @@ var arr1 = [0,1,2,
             6,7,8
            ]
 
+
+
+function draw(){
+    if ( arr.length !== 9){
+        return false
+    }for (let i = 0; i < arr.length; i++){
+        if (arr[i]!== 'X' && arr[i] !== 'O'){
+           return false
+        }
+    }
+    return true
+}
+
 function checkThat(){
 //Horizontal
-        if(arr[0] == arr[1] && arr[0] == arr[2]  && arr[0] != null && arr[1] != null && arr[2] != null){
-            over(curr)            
-        }else if (arr[3] == arr[4] && arr[3] == arr[5] && arr[3] != null && arr[4] != null && arr[5] != null){
+        if(arr[0] == arr[1] && arr[0] == arr[2]  && arr[0] != 1 && arr[1] != 1 && arr[2] != 1){
             over(curr)
-        }else if (arr[6] == arr[7] && arr[6] == arr[8] && arr[6] != null && arr[7] != null && arr[8] != null){
+                   
+        }else if (arr[3] == arr[4] && arr[3] == arr[5] && arr[3] != 1 && arr[4] != 1 && arr[5] != 1){
             over(curr)
+            
+        }else if (arr[6] == arr[7] && arr[6] == arr[8] && arr[6] != 1 && arr[7] != 1 && arr[8] != 1){
+            over(curr)
+           
         }
 
 //vertical
-        else if(arr[0] == arr[3] && arr[0] == arr[6]  && arr[0] != null && arr[3] != null && arr[6] != null){
+        else if(arr[0] == arr[3] && arr[0] == arr[6]  && arr[0] != 1 && arr[3] != 1 && arr[6] != 1){
             over(curr)
-        }else if (arr[1] == arr[4] && arr[1] == arr[7] && arr[1] != null && arr[4] != null && arr[7] != null){
+           
+        }else if (arr[1] == arr[4] && arr[1] == arr[7] && arr[1] != 1 && arr[4] != 1 && arr[7] != 1){
             over(curr)
-        }else if (arr[2] == arr[5] && arr[2] == arr[8] && arr[2] != null && arr[5] != null && arr[8] != null){
+           
+        }else if (arr[2] == arr[5] && arr[2] == arr[8] && arr[2] != 1 && arr[5] != 1 && arr[8] != 1){
             over(curr)
+            
         }
  //Across
-        else if (arr[0] == arr[4] && arr[0] == arr[8] && arr[0] != null && arr[4] != null && arr[8] != null){
+        else if (arr[0] == arr[4] && arr[0] == arr[8] && arr[0] != 1 && arr[4] != 1 && arr[8] != 1){
             over(curr)
-        }else if (arr[2] == arr[4] && arr[2] == arr[6] && arr[2] != null && arr[4] != null && arr[6] != null){
+            
+        }else if (arr[2] == arr[4] && arr[2] == arr[6] && arr[2] != 1 && arr[4] != 1 && arr[6] != 1){
             over(curr)
-        }else if(HasNull(arr)){
-            over("Draw, nobody")
+            
         }
+
+        else if (draw()) {
+             console.log("Draw")
+       over("Draw, Nobody")
+    }
        
 }
 
@@ -56,20 +81,6 @@ function over(text){
 
    
 }
-function HasNull(array){
-    if(arr.length != 9){
-        return false;
-    }
-    for(choice in array){
-        if(choice != "X" && choice != "Y"){
-            return false
-        }
-    }
-
-    return true
-}
-
-
 function choose(index ,obj){
    if(curr == "X"){
     curr = "O"
@@ -85,8 +96,10 @@ function choose(index ,obj){
 obj.setAttribute("disabled" , "true")
 
 checkThat()
-HasNull()
- 
+
+console.log(arr.length)
+console.log(draw())
+
 
 }
 
